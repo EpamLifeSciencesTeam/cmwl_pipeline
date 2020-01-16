85644acb0a25cda6b5214f28169e127b2ed2e59f
Ostap Strashevskii <strashevskyseo@gmail.com>
2020-01-16 15:36:00 +0300

    fix: fix reviewed

--- portal/src/test/scala/cromwell/pipeline/spec/ServiceSpec.scala
+++
@@ -1,7 +0,0 @@
-package cromwell.pipeline.spec
-
-import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{ AsyncWordSpec, Matchers }
-import org.scalatestplus.mockito.MockitoSugar
-
-abstract class ServiceSpec extends AsyncWordSpec with Matchers with MockitoSugar with ScalaFutures
