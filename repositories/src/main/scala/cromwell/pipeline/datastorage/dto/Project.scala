d46b8c0ac561cb0ea40794c00846caa97417e40a
Nikita_Levchenko <nikita0levchenko@gmail.com>
2020-01-22 18:53:13 +0300

    findProjectByName() - is work now

--- repositories/src/main/scala/cromwell/pipeline/datastorage/dto/Project.scala
+++ repositories/src/main/scala/cromwell/pipeline/datastorage/dto/Project.scala
@@ -1,5 +1,7 @@
 package cromwell.pipeline.datastorage.dto
 
+import java.nio.file.Path
+
 import play.api.libs.json.{ Format, Json, OFormat }
 import slick.lifted.MappedTo
 import play.api.libs.functional.syntax._
@@ -27,3 +29,19 @@ final case class ProjectAdditionRequest(name: String)
 object ProjectAdditionRequest {
   implicit lazy val projectAdditionFormat: OFormat[ProjectAdditionRequest] = Json.format[ProjectAdditionRequest]
 }
+
+final case class ProjectDeleteRequest(projectId: ProjectId)
+
+object ProjectDeleteRequest {
+  implicit lazy val projectDeleteFormat: OFormat[ProjectDeleteRequest] = Json.format[ProjectDeleteRequest]
+}
+
+final case class ProjectUpdateRequest(projectId: ProjectId, name: String, repository: String)
+
+object ProjectUpdateRequest {
+  implicit val updateRequestFormat: OFormat[ProjectUpdateRequest] = Json.format[ProjectUpdateRequest]
+}
+
+final case class Version(value: String) extends AnyVal
+
+final case class ProjectFile(path: Path, content: String)
--- repositories/src/main/scala/cromwell/pipeline/datastorage/dto/ProjectResponse.scala
+++ repositories/src/main/scala/cromwell/pipeline/datastorage/dto/ProjectResponse.scala
@@ -1,10 +1,12 @@
 package cromwell.pipeline.datastorage.dto
 
-import play.api.libs.json.{ Json, OFormat }
+import play.api.libs.json.{Json, OFormat}
 
 final case class ProjectResponse(
   projectId: ProjectId,
+  ownerId: UserId,
   name: String,
+  repository: String,
   active: Boolean
 )
 
