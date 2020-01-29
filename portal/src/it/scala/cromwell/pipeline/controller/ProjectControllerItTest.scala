04556c11b90587cdaefb8539892eddf3cb2403ec
Nikita_Levchenko <nikita0levchenko@gmail.com>
2020-01-29 15:38:36 +0300

    fixed small problems

--- portal/src/it/scala/cromwell/pipeline/controller/ProjectControllerItTest.scala
+++ portal/src/it/scala/cromwell/pipeline/controller/ProjectControllerItTest.scala
@@ -1,21 +1,17 @@
 package cromwell.pipeline.controller
 
-import java.util.UUID
-
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import com.dimafeng.testcontainers.{ForAllTestContainer, PostgreSQLContainer}
+import com.dimafeng.testcontainers.{ ForAllTestContainer, PostgreSQLContainer }
 import com.typesafe.config.Config
 import cromwell.pipeline.ApplicationComponents
-import cromwell.pipeline.datastorage.dao.repository.utils.{TestProjectUtils, TestUserUtils}
-import cromwell.pipeline.datastorage.dto.{Project, ProjectId, UserId}
-import cromwell.pipeline.datastorage.utils.auth.AccessTokenContent
-import cromwell.pipeline.utils.TestContainersUtils
+import cromwell.pipeline.datastorage.dto.Project
+import cromwell.pipeline.utils.auth.{ AccessTokenContent, TestContainersUtils, TestProjectUtils, TestUserUtils }
 import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
-import org.scalatest.{AsyncWordSpec, Matchers}
+import org.scalatest.{ AsyncWordSpec, Matchers }
 
 class ProjectControllerItTest
-  extends AsyncWordSpec
+    extends AsyncWordSpec
     with Matchers
     with ScalatestRouteTest
     with PlayJsonSupport
@@ -35,9 +31,8 @@ class ProjectControllerItTest
     "getProjectByName" should {
       "return a project with the same name" in {
         val dummyUser = TestUserUtils.getDummyUser("1")
-        val dummyProject = TestProjectUtils.getDummyProject(ProjectId(UUID.randomUUID().toString), UserId("1"))
+        val dummyProject = TestProjectUtils.getDummyProject("11", "1")
         val projectByNameRequest = dummyProject.name
-
         userRepository
           .addUser(dummyUser)
           .flatMap(
