cc6881a15ffe1af597865e945665f5085764adc7
Nikita_Levchenko <nikita0levchenko@gmail.com>
2020-02-03 04:04:36 +0300

    fixed security hole

--- services/src/main/scala/cromwell/pipeline/service/ProjectService.scala
+++ services/src/main/scala/cromwell/pipeline/service/ProjectService.scala
@@ -2,9 +2,10 @@ package cromwell.pipeline.service
 
 import java.util.UUID
 
+import akka.actor.FSM.Failure
 import cromwell.pipeline.datastorage.dao.repository.ProjectRepository
 import cromwell.pipeline.datastorage.dto.project.ProjectAdditionRequest
-import cromwell.pipeline.datastorage.dto.{ Project, ProjectId }
+import cromwell.pipeline.datastorage.dto.{ Project, ProjectId, UserId }
 
 import scala.concurrent.{ ExecutionContext, Future }
 
@@ -13,8 +14,17 @@ class ProjectService(projectRepository: ProjectRepository)(implicit executionCon
   def getProjectById(projectId: ProjectId): Future[Option[Project]] =
     projectRepository.getProjectById(projectId)
 
-  def getProjectByName(namePattern: String): Future[Option[Project]] =
-    projectRepository.getProjectByName(namePattern)
+  def getProjectByName(namePattern: String, userId: UserId): Future[Option[Project]] =
+    projectRepository.getProjectByName(namePattern).flatMap {
+      case Some(project) =>
+        project match {
+          case project if (project.ownerId == userId) => {
+            projectRepository.getProjectByName(namePattern)
+          }
+          case _ => Future.failed(new RuntimeException("Access denied"))
+        }
+      case None => Future.failed(new RuntimeException("Project does not exist"))
+    }
 
   def addProject(request: ProjectAdditionRequest): Future[ProjectId] = {
     val project =
