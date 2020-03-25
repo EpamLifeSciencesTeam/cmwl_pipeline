a7b8c954f744d40c04c74058d1b81a51c6364bad
Andrew Maximenko <Andrei_Maksimenko@epam.com>
2020-03-26 00:11:14 +0300

    feat: add GitLabProject config
    Move information from local config to global config, and add logic to
    pull this config to services. Fix request logic - move information
    from params to payload. And refactor ProjectVersioning to put execution
    contexts to each method.

--- services/src/main/scala/cromwell/pipeline/service/ServiceModule.scala
+++ services/src/main/scala/cromwell/pipeline/service/ServiceModule.scala
@@ -1,14 +1,18 @@
 package cromwell.pipeline.service
 
 import cromwell.pipeline.datastorage.DatastorageModule
+import cromwell.pipeline.utils.GitLabConfig
 
 import scala.concurrent.ExecutionContext
 
-class ServiceModule(datastorageModule: DatastorageModule)(
+class ServiceModule(datastorageModule: DatastorageModule, config: GitLabConfig)(
   implicit executionContext: ExecutionContext
 ) {
   lazy val authService: AuthService =
     new AuthService(datastorageModule.userRepository, datastorageModule.authUtils)
   lazy val userService: UserService = new UserService(datastorageModule.userRepository)
   lazy val projectService: ProjectService = new ProjectService(datastorageModule.projectRepository)
+  lazy val httpClient: HttpClient = ???
+  lazy val projectVersioning: GitLabProjectVersioning = new GitLabProjectVersioning(httpClient, config)
+
 }
