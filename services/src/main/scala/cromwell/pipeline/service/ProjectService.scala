e6b7f9f5501d1c4ad841cd89d89e5190f5edadf3
Nikita_Levchenko <nikita0levchenko@gmail.com>
2020-04-12 21:51:47 +0300

    style: Fix a project service

--- services/src/main/scala/cromwell/pipeline/service/ProjectService.scala
+++ services/src/main/scala/cromwell/pipeline/service/ProjectService.scala
@@ -3,14 +3,12 @@ package cromwell.pipeline.service
 import java.util.UUID
 
 import cromwell.pipeline.datastorage.dao.repository.ProjectRepository
-import cromwell.pipeline.datastorage.dto.{ Project, ProjectAdditionRequest, ProjectId }
+import cromwell.pipeline.datastorage.dto.{ Project, ProjectAdditionRequest, ProjectId, ProjectUpdateRequest, UserId }
 import cromwell.pipeline.service.Exceptions.{ ProjectAccessDeniedException, ProjectNotFoundException }
 
 import scala.concurrent.{ ExecutionContext, Future }
 
 class ProjectService(projectRepository: ProjectRepository)(implicit executionContext: ExecutionContext) {
-
-  import cromwell.pipeline.datastorage.dto.{ ProjectUpdateRequest, UserId }
 
   def getProjectById(projectId: ProjectId): Future[Option[Project]] =
     projectRepository.getProjectById(projectId)
