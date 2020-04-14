ad5f318a7f5478f8c08c798fcf4a5f6859441a76
Grigorii Berezin <grigorii_berezin@epam.com>
2020-04-14 14:41:18 +0300

    feat: add endpoint for upload file

--- services/src/test/scala/cromwell/pipeline/service/ProjectFileServiceTest.scala
+++ services/src/test/scala/cromwell/pipeline/service/ProjectFileServiceTest.scala
@@ -1,18 +1,24 @@
 package cromwell.pipeline.service
 
+import java.nio.file.Paths
+
 import cats.data.NonEmptyList
-import cromwell.pipeline.datastorage.dto.{ FileContent, ValidationError }
+import cromwell.pipeline.datastorage.dao.repository.utils.TestProjectUtils
+import cromwell.pipeline.datastorage.dto.{ FileContent, ProjectFile, ValidationError }
 import cromwell.pipeline.womtool.WomTool
-import org.mockito.Mockito.when
 import org.mockito.Matchers.any
+import org.mockito.Mockito.when
 import org.scalatest.{ AsyncWordSpec, Matchers }
 import org.scalatestplus.mockito.MockitoSugar
 import wom.executable.WomBundle
 
+import scala.concurrent.Future
+
 class ProjectFileServiceTest extends AsyncWordSpec with Matchers with MockitoSugar {
 
   private val womTool = mock[WomTool]
-  private val projectFileService = new ProjectFileService(womTool)
+  private val projectVersioning = mock[ProjectVersioning[VersioningException]]
+  private val projectFileService = new ProjectFileService(womTool, projectVersioning)
 
   private val correctWdl = "task hello {}"
   private val incorrectWdl = "task hello {"
@@ -22,16 +28,30 @@ class ProjectFileServiceTest extends AsyncWordSpec with Matchers with MockitoSug
     "validateFile" should {
       "return valid message to valid file" taggedAs Service in {
         val request = FileContent(correctWdl)
-
         when(womTool.validate(correctWdl)).thenReturn(Right(any[WomBundle]))
         projectFileService.validateFile(request).map(_ shouldBe Right(()))
       }
 
       "return error message to incorrect file" taggedAs Service in {
         val request = FileContent(incorrectWdl)
-
         when(womTool.validate(incorrectWdl)).thenReturn(Left(NonEmptyList(errorMessage, Nil)))
         projectFileService.validateFile(request).map(_ shouldBe Left(ValidationError(List(errorMessage))))
+      }
+    }
+
+    "upload file" should {
+      val project = TestProjectUtils.getDummyProject()
+      val projectFile = ProjectFile(Paths.get("test.txt"), "File content")
+
+      "return success message for request" taggedAs Service in {
+        when(projectVersioning.updateFile(project, projectFile)).thenReturn(Future.successful(Right("Success")))
+        projectFileService.uploadFile(project, projectFile).map(_ shouldBe Right("Success"))
+      }
+
+      "return error message for error request" taggedAs Service in {
+        when(projectVersioning.updateFile(project, projectFile))
+          .thenReturn(Future.successful(Left(VersioningException("Something wrong"))))
+        projectFileService.uploadFile(project, projectFile).map(_ shouldBe Left(VersioningException("Something wrong")))
       }
     }
   }
