148271892168f82e1f6517c0d15c28b541f06f4b
Ostap Strashevskii <Ostap_Strashevskii@epam.com>
2020-04-22 14:49:09 +0300

    conflict resolving

--- controllers/src/main/scala/cromwell/pipeline/controller/ProjectFileController.scala
+++ controllers/src/main/scala/cromwell/pipeline/controller/ProjectFileController.scala
@@ -4,7 +4,7 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import cromwell.pipeline.datastorage.dto.{ FileContent, ProjectUpdateFileRequest, ValidationError }
-import cromwell.pipeline.datastorage.utils.auth.AccessTokenContent
+import cromwell.pipeline.utils.auth.AccessTokenContent
 import cromwell.pipeline.service.ProjectFileService
 import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport._
 
