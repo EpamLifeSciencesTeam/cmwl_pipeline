7f31b58a6b96d98bc60ac3e07d46178097f4c372
Manvicha <manvicha@MacBook-Pro-de-Manvicha.local>
2020-05-13 07:05:36 -0400

    refactor: Change logic to use for comprehension

--- controllers/src/main/scala/cromwell/pipeline/controller/ProjectFileController.scala
+++ controllers/src/main/scala/cromwell/pipeline/controller/ProjectFileController.scala
@@ -29,13 +29,25 @@ class ProjectFileController(wdlService: ProjectFileService)(implicit val executi
         post {
           entity(as[ProjectUpdateFileRequest]) {
             request =>
-              onComplete(wdlService.validateFile(FileContent(request.projectFile.content))) {
+              val result = for {
+                validateResponse <- wdlService.validateFile(FileContent(request.projectFile.content))
+                uploadResponse <- wdlService.uploadFile(request.project, request.projectFile)
+              } yield {
+                if (validateResponse.isRight && uploadResponse.isRight) {
+                  StatusCodes.OK.intValue -> uploadResponse.right.get
+                } else if (validateResponse.isLeft && uploadResponse.isRight) {
+                  StatusCodes.Created.intValue -> uploadResponse.right.get
+                } else if (uploadResponse.isLeft) {
+                  StatusCodes.ImATeapot.intValue -> uploadResponse.left.get.message
+                }
+              }
+
+              while (!result.isCompleted) {}
+
+              result.value.get match {
                 case Success(value) =>
-                  val validationStatus = value.fold(ve => false, un => true)
-                  onComplete(wdlService.uploadFile(request.project, request.projectFile)) {
-                    case Success(Left(e)) => complete(StatusCodes.ImATeapot, e.getMessage) // TODO: change status code
-                    case Success(_)       => if (validationStatus) complete(StatusCodes.OK) else complete(StatusCodes.Created)
-                    case Failure(e)       => complete(StatusCodes.InternalServerError, e.getMessage)
+                  value match {
+                    case tuple @ (status: Int, message: String) => complete(status, message)
                   }
                 case Failure(e) => complete(StatusCodes.InternalServerError, e.getMessage)
               }
