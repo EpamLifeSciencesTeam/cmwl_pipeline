61adfc02010d285f12e4c43e6aed53efdee0b838
Manvicha <manvicha@MacBook-Pro-de-Manvicha.local>
2020-07-13 16:09:50 -0400

    fix: Change case from ProjectFileController

--- controllers/src/main/scala/cromwell/pipeline/controller/ProjectFileController.scala
+++ controllers/src/main/scala/cromwell/pipeline/controller/ProjectFileController.scala
@@ -4,12 +4,13 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import cromwell.pipeline.datastorage.dto.auth.AccessTokenContent
-import cromwell.pipeline.datastorage.dto.{ FileContent, ProjectBuildConfigurationRequest, ProjectUpdateFileRequest }
-import cromwell.pipeline.service.ProjectFileService
+import cromwell.pipeline.datastorage.dto.{FileContent, ProjectBuildConfigurationRequest, ProjectUpdateFileRequest}
+import cromwell.pipeline.service.VersioningException.{FileException, GitException, HttpException, ProjectException, RepositoryException}
+import cromwell.pipeline.service.{ProjectFileService, VersioningException}
 import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport._
 
 import scala.concurrent.ExecutionContext
-import scala.util.{ Failure, Success }
+import scala.util.{Failure, Success}
 
 class ProjectFileController(wdlService: ProjectFileService)(
   implicit val executionContext: ExecutionContext
@@ -38,7 +39,10 @@ class ProjectFileController(wdlService: ProjectFileService)(
                 (validateResponse, uploadResponse) match {
                   case (Right(_), Right(response)) => StatusCodes.OK.intValue -> response
                   case (Left(_), Right(response))  => StatusCodes.Created.intValue -> response
-                  case (_, Left(response))         => StatusCodes.UnprocessableEntity.intValue -> response.getMessage
+                  case (_, Left(response))         => response match {
+                    case HttpException(message) => StatusCodes.UnprocessableEntity.intValue -> message
+                    case _ => StatusCodes.UnprocessableEntity.intValue -> "Bad request"
+                  }
                 }
               }) {
                 case Success((status, message)) => complete(status, message)
