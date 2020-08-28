9fd5547ab8f9eca0867edfffbc468ac16c5b7cfa
Dmitrii Shevchenko <Dmitrii_Shevchenko@epam.com>
2020-08-28 11:32:01 +0300

    fix: exception style

--- services/src/main/scala/cromwell/pipeline/service/VersioningException.scala
+++ services/src/main/scala/cromwell/pipeline/service/VersioningException.scala
@@ -1,16 +1,13 @@
 package cromwell.pipeline.service
 
-sealed abstract class VersioningException extends Exception {
-  val message: String
-  override def getMessage() = message
-}
+sealed abstract class VersioningException(message: String) extends Exception(message)
 
 object VersioningException {
 
-  case class ProjectException(message: String) extends VersioningException()
-  case class FileException(message: String) extends VersioningException()
-  case class GitException(message: String) extends VersioningException()
-  case class HttpException(message: String) extends VersioningException()
-  case class RepositoryException(message: String) extends VersioningException()
+  case class ProjectException(message: String) extends VersioningException(message)
+  case class FileException(message: String) extends VersioningException(message)
+  case class GitException(message: String) extends VersioningException(message)
+  case class HttpException(message: String) extends VersioningException(message)
+  case class RepositoryException(message: String) extends VersioningException(message)
 
 }
