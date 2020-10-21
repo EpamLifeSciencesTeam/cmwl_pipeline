cdb69a2179bcfdce868f3c21a1ca0423be72e3dc
Iaroslav Tavchenkov <iaroslav_tavchenkov@epam.com>
2020-10-21 13:47:40 +0300

    ref: add disabled log for Mongo

--- datasource/src/main/scala/cromwell/pipeline/database/MongoEngine.scala
+++ datasource/src/main/scala/cromwell/pipeline/database/MongoEngine.scala
@@ -1,5 +1,6 @@
 package cromwell.pipeline.database
 
+//import cromwell.pipeline.database.MongoEngine.log
 import cromwell.pipeline.utils.MongoConfig
 import org.mongodb.scala.{
   Document,
@@ -10,6 +11,7 @@ import org.mongodb.scala.{
   MongoDatabase,
   ServerAddress
 }
+import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConverters._
 
@@ -28,10 +30,19 @@ class MongoEngine(mongoConfig: MongoConfig) extends AutoCloseable {
     .credential(mongoCredential)
     .build()
   lazy val mongoClient: MongoClient = MongoClient(mongoSettings)
+//  log.info(s"Mongo connection $mongoClient")
   lazy val mongoDatabase: MongoDatabase =
     mongoClient.getDatabase(mongoConfig.database)
   lazy val mongoCollection: MongoCollection[Document] =
     mongoDatabase.getCollection(mongoConfig.collection)
 
   override def close(): Unit = mongoClient.close()
+}
+
+object MongoEngine {
+////  val log = LoggerFactory.getLogger(this.getClass)
+//  val log = LoggerFactory.getLogger(MongoEngine.getClass)
+////  log.info("Mongo connection" MongoEngine.mongoClient)
+//  log.debug(s"reading $MongoEngine")
+////  log.error(s"bad request: $req")
 }
