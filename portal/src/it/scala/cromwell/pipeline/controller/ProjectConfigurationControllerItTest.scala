5710fbfeeee7724150f130b9a60c5ee2e62dc2f4
Iaroslav Tavchenkov <iaroslav_tavchenkov@epam.com>
2020-11-02 12:40:16 +0300

    feat: add integrations test

--- portal/src/it/scala/cromwell/pipeline/controller/ProjectConfigurationControllerItTest.scala
+++ portal/src/it/scala/cromwell/pipeline/controller/ProjectConfigurationControllerItTest.scala
@@ -1,9 +1,18 @@
 package cromwell.pipeline.controller
 
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.dimafeng.testcontainers.{ ForAllTestContainer, MongoDBContainer }
 import com.typesafe.config.Config
 import cromwell.pipeline.ApplicationComponents
+import cromwell.pipeline.datastorage.dao.repository.utils.{
+  TestProjectConfigurationUtils,
+  TestProjectUtils,
+  TestUserUtils
+}
+import cromwell.pipeline.datastorage.dto.ProjectConfiguration.toDocument
+import cromwell.pipeline.datastorage.dto.auth.AccessTokenContent
+import cromwell.pipeline.datastorage.dto.{ ProjectConfiguration, User }
 import cromwell.pipeline.utils.TestContainersUtils
 import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
 import org.scalatest.{ AsyncWordSpec, Matchers }
@@ -22,8 +31,32 @@ class ProjectConfigurationControllerItTest
   import components.controllerModule.configurationController
   import components.datastorageModule.configurationRepository
 
+  val projectConfiguration = TestProjectConfigurationUtils.projectConfigurationGen.sample.get
+  val doc = ProjectConfiguration.toDocument(projectConfiguration)
+  val id = projectConfiguration.projectId
+
+  println("hello")
+
   override protected def beforeAll(): Unit = {
     super.beforeAll()
     components.datastorageModule.pipelineDatabaseEngine.updateSchema()
+    val x = configurationRepository.addOne(doc)
   }
+
+  "project configuration controller" when {
+    "add some project configuration" should {
+      "return OK status code" in {
+        val dummyUser: User = TestUserUtils.getDummyUser()
+        val dummyProject = TestProjectUtils.getDummyProject()
+        val projectConfiguration = ProjectConfiguration(dummyProject.projectId, List())
+        val accessToken = AccessTokenContent(dummyUser.userId)
+        configurationRepository.addOne(toDocument(projectConfiguration)).flatMap { _ =>
+          Get("/configurations?=" + dummyProject.projectId) ~> configurationController.route(accessToken) ~> check {
+            status shouldBe StatusCodes.OK
+          }
+        }
+      }
+    }
+  }
+
 }
