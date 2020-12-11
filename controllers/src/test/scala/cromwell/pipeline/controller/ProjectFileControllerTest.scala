8dec83f07a90796c07175afbc0627d3c95a41fde
Iaroslav Tavchenkov <iaroslav_tavchenkov@epam.com>
2020-12-11 12:45:51 +0300

    second ProjectFileContent type fix

--- controllers/src/test/scala/cromwell/pipeline/controller/ProjectFileControllerTest.scala
+++ controllers/src/test/scala/cromwell/pipeline/controller/ProjectFileControllerTest.scala
@@ -5,14 +5,14 @@ import java.nio.file.Paths
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import cromwell.pipeline.datastorage.dao.repository.utils.{TestProjectUtils, TestUserUtils}
+import cromwell.pipeline.datastorage.dao.repository.utils.{ TestProjectUtils, TestUserUtils }
 import cromwell.pipeline.datastorage.dto.auth.AccessTokenContent
 import cromwell.pipeline.datastorage.dto._
-import cromwell.pipeline.service.{ProjectFileService, ProjectService, VersioningException}
-import cromwell.pipeline.utils.{ApplicationConfig, GitLabConfig}
+import cromwell.pipeline.service.{ ProjectFileService, ProjectService, VersioningException }
+import cromwell.pipeline.utils.{ ApplicationConfig, GitLabConfig }
 import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport._
 import org.mockito.Mockito.when
-import org.scalatest.{AsyncWordSpec, Matchers}
+import org.scalatest.{ AsyncWordSpec, Matchers }
 import org.scalatestplus.mockito.MockitoSugar
 
 import scala.concurrent.Future
@@ -88,7 +88,7 @@ class ProjectFileControllerTest extends AsyncWordSpec with Matchers with Scalate
     "delete file" should {
       val project = TestProjectUtils.getDummyProject()
       val branchName: String = gitLabConfig.defaultBranch
-      val projectFile = ProjectFile(Paths.get("folder/test.txt"), "file context")
+      val projectFile = ProjectFile(Paths.get("folder/test.txt"), ProjectFileContent("file context"))
       val path = URLEncoder.encode(projectFile.path.toString, "UTF-8")
       val commitMessage = s"$path file has been deleted from $branchName"
       val commitMessageUrl = URLEncoder.encode(commitMessage, "UTF-8")
@@ -108,7 +108,7 @@ class ProjectFileControllerTest extends AsyncWordSpec with Matchers with Scalate
 
     "get file" should {
       val project = TestProjectUtils.getDummyProject()
-      val projectFile = ProjectFile(Paths.get("folder/test.txt"), "file context")
+      val projectFile = ProjectFile(Paths.get("folder/test.txt"), ProjectFileContent("file context"))
       val path = URLEncoder.encode(projectFile.path.toString, "UTF-8")
       val version = PipelineVersion("v0.0.2")
       val accessToken = AccessTokenContent(TestUserUtils.getDummyUserId)
