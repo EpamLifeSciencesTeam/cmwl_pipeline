3c7738d993f99b0bd15d9cff30307c84fb47643e
Aleksei Eschenko <aleksei_eschenko@epam.com>
2021-04-13 09:43:45 +0300

    feat: implement aggregation resources service

---
+++ repositories/src/main/scala/cromwell/pipeline/datastorage/dto/CromwellInput.scala
@@ -0,0 +1,11 @@
+package cromwell.pipeline.datastorage.dto
+
+import cromwell.pipeline.model.wrapper.UserId
+
+final case class CromwellInput(
+  projectId: ProjectId,
+  userId: UserId,
+  projectVersion: PipelineVersion,
+  files: List[ProjectFile],
+  configurations: List[ProjectFileConfiguration]
+)
---
+++ services/src/main/scala/cromwell/pipeline/service/AggregationService.scala
@@ -0,0 +1,52 @@
+package cromwell.pipeline.service
+
+import cats.data.EitherT
+import cats.implicits._
+import cromwell.pipeline.datastorage.dto.{ CromwellInput, PipelineVersion, Project, ProjectFile, ProjectId, Run }
+import cromwell.pipeline.model.wrapper.UserId
+
+import java.nio.file.{ Path, Paths }
+import scala.concurrent.{ ExecutionContext, Future }
+
+class AggregationService(
+  projectService: ProjectService,
+  projectVersioning: ProjectVersioning[VersioningException],
+  projectConfigurationService: ProjectConfigurationService
+)(implicit executionContext: ExecutionContext) {
+
+  def aggregate(run: Run): Future[CromwellInput] = {
+    val version = PipelineVersion(run.projectVersion)
+    val projectFiles =
+      getFilesByProjectId(run.projectId, run.userId, Some(version)).valueOrF(e => Future.failed(e))
+    val futureFileConfigurations = projectConfigurationService.getById(run.projectId).flatMap {
+      case Some(config) => Future.successful(config.projectFileConfigurations)
+      case None         => Future.failed(new RuntimeException("Configurations for projectId " + run.projectId + " not found"))
+    }
+    for {
+      files <- projectFiles
+      configs <- futureFileConfigurations
+    } yield CromwellInput(run.projectId, run.userId, version, files, configs)
+  }
+
+  private def getFilesByProjectId(
+    projectId: ProjectId,
+    userId: UserId,
+    version: Option[PipelineVersion]
+  ): EitherT[Future, VersioningException, List[ProjectFile]] = {
+
+    type EitherF[T] = EitherT[Future, VersioningException, T]
+
+    def getProjectFile(
+      project: Project,
+      path: Path,
+      version: Option[PipelineVersion]
+    ): EitherF[ProjectFile] =
+      EitherT(projectVersioning.getFile(project, path, version))
+
+    for {
+      project <- EitherT.right(projectService.getUserProjectById(projectId, userId))
+      trees <- EitherT(projectVersioning.getFilesTree(project, version))
+      files <- trees.toList.traverse(tree => getProjectFile(project, Paths.get(tree.path), version))
+    } yield files
+  }
+}
