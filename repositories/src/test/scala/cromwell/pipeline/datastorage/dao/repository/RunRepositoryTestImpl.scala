7cf5d20a3018748e09273e3ae84995fa4697d85d
Oleg Koshelev <etukoshelevoa@gmail.com>
2021-06-30 11:58:27 +0300

    test: Add test implementation of repository classes

---
+++ repositories/src/test/scala/cromwell/pipeline/datastorage/dao/repository/RunRepositoryTestImpl.scala
@@ -0,0 +1,44 @@
+package cromwell.pipeline.datastorage.dao.repository
+
+import cromwell.pipeline.datastorage.dto.Run
+import cromwell.pipeline.model.wrapper.{ RunId, UserId }
+import scala.collection.mutable
+import scala.concurrent.Future
+
+class RunRepositoryTestImpl extends RunRepository {
+
+  private val runs: mutable.Map[RunId, Run] = mutable.Map.empty
+
+  def getRunByIdAndUser(runId: RunId, userId: UserId): Future[Option[Run]] = {
+    val run = for {
+      (id, entity) <- runs if id == runId && entity.userId == userId
+    } yield entity
+    Future.successful(run.headOption)
+  }
+
+  def deleteRunById(runId: RunId): Future[Int] = {
+    runs -= runId
+    Future.successful(0)
+  }
+
+  def addRun(run: Run): Future[RunId] = {
+    runs += (run.runId -> run)
+    Future.successful(run.runId)
+  }
+
+  def updateRun(updatedRun: Run): Future[Int] = {
+    if (runs.contains(updatedRun.runId)) runs += (updatedRun.runId -> updatedRun)
+    Future.successful(0)
+  }
+
+}
+
+object RunRepositoryTestImpl {
+
+  def apply(runs: Run*): RunRepositoryTestImpl = {
+    val runRepositoryTestImpl = new RunRepositoryTestImpl
+    runs.foreach(runRepositoryTestImpl.addRun)
+    runRepositoryTestImpl
+  }
+
+}
---
+++ repositories/src/test/scala/cromwell/pipeline/datastorage/dao/repository/UserRepositoryTestImp.scala
@@ -0,0 +1,59 @@
+package cromwell.pipeline.datastorage.dao.repository
+
+import cromwell.pipeline.datastorage.dto.User
+import cromwell.pipeline.model.wrapper.{ UserEmail, UserId }
+import scala.collection.mutable
+import scala.concurrent.Future
+
+class UserRepositoryTestImp extends UserRepository {
+
+  private val users: mutable.Map[UserId, User] = mutable.Map.empty
+
+  def getUserById(userId: UserId): Future[Option[User]] =
+    Future.successful(users.get(userId))
+
+  def getUserByEmail(email: UserEmail): Future[Option[User]] =
+    Future.successful(users.values.find(_.email == email))
+
+  def getUsersByEmail(emailPattern: String): Future[Seq[User]] =
+    Future.successful(users.values.filter(_.email.unwrap.contains(emailPattern)).toSeq)
+
+  def addUser(user: User): Future[UserId] = {
+    users += (user.userId -> user)
+    Future.successful(user.userId)
+  }
+
+  def deactivateUserByEmail(email: UserEmail): Future[Int] =
+    deactivateUser[UserEmail](email, user => user.email)
+
+  def deactivateUserById(userId: UserId): Future[Int] =
+    deactivateUser[UserId](userId, user => user.userId)
+
+  private def deactivateUser[A](userParam: A, toUserField: User => A): Future[Int] = {
+    for {
+      (_, user) <- users if userParam == toUserField(user)
+      deactivatedUser = user.copy(active = false)
+    } yield users += (deactivatedUser.userId -> deactivatedUser)
+    Future.successful(0)
+  }
+
+  def updateUser(updatedUser: User): Future[Int] = update(updatedUser)
+
+  def updatePassword(updatedUser: User): Future[Int] = update(updatedUser)
+
+  private def update(updatedUser: User): Future[Int] = {
+    if (users.contains(updatedUser.userId)) users += (updatedUser.userId -> updatedUser)
+    Future.successful(0)
+  }
+
+}
+
+object UserRepositoryTestImp {
+
+  def apply(users: User*): UserRepositoryTestImp = {
+    val userRepositoryTestImp = new UserRepositoryTestImp
+    users.foreach(userRepositoryTestImp.addUser)
+    userRepositoryTestImp
+  }
+
+}
