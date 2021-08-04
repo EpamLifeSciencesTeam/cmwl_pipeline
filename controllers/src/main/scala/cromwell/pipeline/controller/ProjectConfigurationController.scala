dbdcca6819829a2396f8efc10347253de9e24306
DrzuhininPavel <druzini@gmail.com>
2021-08-04 12:59:54 +0300

    feat: Changed ep in ConfigurationController to RESTful

--- controllers/src/main/scala/cromwell/pipeline/controller/ProjectConfigurationController.scala
+++ controllers/src/main/scala/cromwell/pipeline/controller/ProjectConfigurationController.scala
@@ -3,8 +3,9 @@ package cromwell.pipeline.controller
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import cromwell.pipeline.controller.utils.PathMatchers.ProjectId
+import cromwell.pipeline.datastorage.dto._
 import cromwell.pipeline.datastorage.dto.auth.AccessTokenContent
-import cromwell.pipeline.datastorage.dto.{ ProjectConfiguration, ProjectId }
 import cromwell.pipeline.service.ProjectConfigurationService
 import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport._
 
@@ -15,39 +16,42 @@ class ProjectConfigurationController(projectConfigurationService: ProjectConfigu
   implicit val ec: ExecutionContext
 ) {
 
-  private def addConfiguration(implicit accessToken: AccessTokenContent): Route = put {
-    entity(as[ProjectConfiguration]) { request =>
-      onComplete(projectConfigurationService.addConfiguration(request, accessToken.userId)) {
+  private def addConfiguration(projectId: ProjectId)(implicit accessToken: AccessTokenContent): Route = put {
+    entity(as[ProjectConfigurationAdditionRequest]) { request =>
+      val newProjectConfiguration = ProjectConfiguration(
+        id = request.id,
+        projectId = projectId,
+        active = request.active,
+        projectFileConfigurations = request.projectFileConfigurations,
+        version = request.version
+      )
+      onComplete(projectConfigurationService.addConfiguration(newProjectConfiguration, accessToken.userId)) {
         case Failure(e) => complete(StatusCodes.InternalServerError, e.getMessage)
         case Success(_) => complete(StatusCodes.OK)
       }
     }
   }
 
-  private def getConfiguration(implicit accessToken: AccessTokenContent): Route = get {
-    parameter('project_id.as[String]) { projectId =>
-      onComplete(projectConfigurationService.getLastByProjectId(ProjectId(projectId), accessToken.userId)) {
-        case Failure(e)                   => complete(StatusCodes.InternalServerError, e.getMessage)
-        case Success(Some(configuration)) => complete(configuration)
-        case Success(None) =>
-          complete(StatusCodes.NotFound, s"There is no configuration with project_id: $projectId")
-      }
+  private def getConfiguration(projectId: ProjectId)(implicit accessToken: AccessTokenContent): Route = get {
+    onComplete(projectConfigurationService.getLastByProjectId(projectId, accessToken.userId)) {
+      case Failure(e)                   => complete(StatusCodes.InternalServerError, e.getMessage)
+      case Success(Some(configuration)) => complete(configuration)
+      case Success(None) =>
+        complete(StatusCodes.NotFound, s"There is no configuration with project_id: ${projectId.value}")
     }
   }
 
-  private def deactivateConfiguration(implicit accessToken: AccessTokenContent): Route = delete {
-    parameter('project_id.as[String]) { projectId =>
-      onComplete(projectConfigurationService.deactivateLastByProjectId(ProjectId(projectId), accessToken.userId)) {
-        case Failure(e) => complete(StatusCodes.InternalServerError, e.getMessage)
-        case Success(_) => complete(StatusCodes.NoContent)
-      }
+  private def deactivateConfiguration(projectId: ProjectId)(implicit accessToken: AccessTokenContent): Route = delete {
+    onComplete(projectConfigurationService.deactivateLastByProjectId(projectId, accessToken.userId)) {
+      case Failure(e) => complete(StatusCodes.InternalServerError, e.getMessage)
+      case Success(_) => complete(StatusCodes.NoContent)
     }
   }
 
   val route: AccessTokenContent => Route = implicit accessToken =>
-    pathPrefix("configurations") {
-      addConfiguration ~
-      getConfiguration ~
-      deactivateConfiguration
+    pathPrefix("projects" / ProjectId / "configurations") { projectId =>
+      addConfiguration(projectId) ~
+      getConfiguration(projectId) ~
+      deactivateConfiguration(projectId)
     }
 }
--- controllers/src/test/scala/cromwell/pipeline/controller/ProjectConfigurationControllerTest.scala
+++ controllers/src/test/scala/cromwell/pipeline/controller/ProjectConfigurationControllerTest.scala
@@ -1,7 +1,5 @@
 package cromwell.pipeline.controller
 
-import java.nio.file.Paths
-
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import cromwell.pipeline.datastorage.dao.utils.{ TestProjectUtils, TestUserUtils }
@@ -13,6 +11,7 @@ import org.mockito.Mockito.when
 import org.scalatest.{ AsyncWordSpec, Matchers }
 import org.scalatestplus.mockito.MockitoSugar
 
+import java.nio.file.Paths
 import scala.concurrent.Future
 
 class ProjectConfigurationControllerTest extends AsyncWordSpec with Matchers with ScalatestRouteTest with MockitoSugar {
@@ -21,14 +20,21 @@ class ProjectConfigurationControllerTest extends AsyncWordSpec with Matchers wit
 
   "ProjectConfigurationController" when {
     val accessToken = AccessTokenContent(TestUserUtils.getDummyUserId)
+    val projectId = TestProjectUtils.getDummyProjectId
     val configuration = ProjectConfiguration(
       ProjectConfigurationId.randomId,
-      TestProjectUtils.getDummyProjectId,
+      projectId,
       active = true,
       List(
         ProjectFileConfiguration(Paths.get("/home/file"), List(FileParameter("nodeName", StringTyped(Some("hello")))))
       ),
       ProjectConfigurationVersion.defaultVersion
+    )
+    val configurationAdditionRequest = ProjectConfigurationAdditionRequest(
+      id = configuration.id,
+      active = configuration.active,
+      projectFileConfigurations = configuration.projectFileConfigurations,
+      version = configuration.version
     )
 
     "update configuration" should {
@@ -36,14 +42,20 @@ class ProjectConfigurationControllerTest extends AsyncWordSpec with Matchers wit
 
       "return success for update configuration" in {
         when(configurationService.addConfiguration(configuration, accessToken.userId)).thenReturn(Future.unit)
-        Put("/configurations", configuration) ~> configurationController.route(accessToken) ~> check {
+        Put(s"/projects/${projectId.value}/configurations", configurationAdditionRequest) ~> configurationController
+          .route(
+            accessToken
+          ) ~> check {
           status shouldBe StatusCodes.OK
         }
       }
 
       "return InternalServerError when failure update configuration" in {
         when(configurationService.addConfiguration(configuration, accessToken.userId)).thenReturn(Future.failed(error))
-        Put("/configurations", configuration) ~> configurationController.route(accessToken) ~> check {
+        Put(s"/projects/${projectId.value}/configurations", configurationAdditionRequest) ~> configurationController
+          .route(
+            accessToken
+          ) ~> check {
           status shouldBe StatusCodes.InternalServerError
           entityAs[String] shouldBe "Something went wrong"
         }
@@ -51,12 +63,10 @@ class ProjectConfigurationControllerTest extends AsyncWordSpec with Matchers wit
     }
 
     "get configuration by project id" should {
-      val projectId = TestProjectUtils.getDummyProjectId
-
       "return configuration by existing project id" in {
         when(configurationService.getLastByProjectId(projectId, accessToken.userId))
           .thenReturn(Future.successful(Some(configuration)))
-        Get("/configurations?project_id=" + projectId.value) ~> configurationController.route(accessToken) ~> check {
+        Get(s"/projects/${projectId.value}/configurations") ~> configurationController.route(accessToken) ~> check {
           status shouldBe StatusCodes.OK
           entityAs[ProjectConfiguration] shouldBe configuration
         }
@@ -64,7 +74,7 @@ class ProjectConfigurationControllerTest extends AsyncWordSpec with Matchers wit
 
       "return message about no project with this project id" in {
         when(configurationService.getLastByProjectId(projectId, accessToken.userId)).thenReturn(Future.successful(None))
-        Get("/configurations?project_id=" + projectId.value) ~> configurationController.route(accessToken) ~> check {
+        Get(s"/projects/${projectId.value}/configurations") ~> configurationController.route(accessToken) ~> check {
           status shouldBe StatusCodes.NotFound
           entityAs[String] shouldBe s"There is no configuration with project_id: ${projectId.value}"
         }
@@ -73,11 +83,10 @@ class ProjectConfigurationControllerTest extends AsyncWordSpec with Matchers wit
 
     "deactivate configuration" should {
       val error = new RuntimeException("Something went wrong")
-      val projectId = TestProjectUtils.getDummyProjectId
 
       "return success for deactivate configuration" in {
         when(configurationService.deactivateLastByProjectId(projectId, accessToken.userId)).thenReturn(Future.unit)
-        Delete("/configurations?project_id=" + projectId.value) ~> configurationController.route(accessToken) ~> check {
+        Delete(s"/projects/${projectId.value}/configurations") ~> configurationController.route(accessToken) ~> check {
           status shouldBe StatusCodes.NoContent
         }
       }
@@ -85,7 +94,7 @@ class ProjectConfigurationControllerTest extends AsyncWordSpec with Matchers wit
       "return InternalServerError when failure deactivate configuration" in {
         when(configurationService.deactivateLastByProjectId(projectId, accessToken.userId))
           .thenReturn(Future.failed(error))
-        Delete("/configurations?project_id=" + projectId.value) ~> configurationController.route(accessToken) ~> check {
+        Delete(s"/projects/${projectId.value}/configurations") ~> configurationController.route(accessToken) ~> check {
           status shouldBe StatusCodes.InternalServerError
           entityAs[String] shouldBe "Something went wrong"
         }
