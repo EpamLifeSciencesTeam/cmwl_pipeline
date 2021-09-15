6ed5253e0936e10d06e5a6cddf47b020a0cac9fd
Georgii Minasian <vasp1ru@gmail.com>
2021-09-15 10:52:28 +0300

    feat:Nothing is working yet

--- controllers/src/main/scala/cromwell/pipeline/controller/ProjectFileController.scala
+++ controllers/src/main/scala/cromwell/pipeline/controller/ProjectFileController.scala
@@ -9,11 +9,12 @@ import cromwell.pipeline.controller.utils.FromStringUnmarshallers._
 import cromwell.pipeline.controller.utils.PathMatchers.{ Path, ProjectId }
 import cromwell.pipeline.datastorage.dto._
 import cromwell.pipeline.datastorage.dto.auth.AccessTokenContent
-import cromwell.pipeline.service.ProjectFileService
+import cromwell.pipeline.service.{ ProjectFileService, VersioningException }
 import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport._
 
 import java.nio.file._
-import scala.concurrent.ExecutionContext
+import scala.collection.immutable
+import scala.concurrent.{ ExecutionContext, Future }
 import scala.util.{ Failure, Success }
 
 class ProjectFileController(wdlService: ProjectFileService)(
@@ -54,7 +55,6 @@ class ProjectFileController(wdlService: ProjectFileService)(
   }
 
   private def uploadFile(projectId: ProjectId)(implicit accessToken: AccessTokenContent): Route = post {
-
     formFields('path.as[Path], 'version.as[PipelineVersion].optional) { (path, version) =>
       fileUpload("file") {
         case (_, byteSource) =>
@@ -86,11 +86,97 @@ class ProjectFileController(wdlService: ProjectFileService)(
     }
   }
 
+  private def uploadAllFiles(projectId: ProjectId)(implicit accessToken: AccessTokenContent): Route = post {
+    case class FilePathAndResponse(
+                                    path: Path,
+                                    validateResponse: Either[ValidationError, Unit],
+                                    uploadResponse: Either[VersioningException, Unit]
+                                  )
+
+    formField('version.as[PipelineVersion].optional) { version =>
+      fileUploadAll("file") { byteSources =>
+        formField('path.as[Path]) { path =>
+          byteSources.map { byteSource =>
+            val contentF: Future[String] = byteSource._2.map(_.utf8String).runFold("")(_ + _)
+            val uploadedFileF: Future[ProjectFile] = contentF.map(x => ProjectFile(path, ProjectFileContent(x)))
+
+            onComplete {
+              uploadedFileF.map { uploadedFile =>
+                for {
+                  validateResponse <- wdlService.validateFile(uploadedFile.content)
+                  uploadResponse <- wdlService.uploadFile(
+                    projectId,
+                    uploadedFile,
+                    version,
+                    accessToken.userId
+                  )
+                } yield (validateResponse, uploadResponse) match {
+                  case (Right(_), Right(_)) => Right(StatusCodes.OK)
+                  case (Left(_), Right(_)) => Right(StatusCodes.Created)
+                  case (_, Left(response)) => Left(response.getMessage)
+                }
+              }
+            }
+          } {
+            case _ => complete(StatusCodes.OK)
+          }
+        }
+      }
+    }
+  }
+
+//  private def uploadAllFiles(projectId: ProjectId)(implicit accessToken: AccessTokenContent): Route = post {
+//
+//    case class FilePathAndResponse(
+//      path: Path,
+//      validateResponse: Either[ValidationError, Unit],
+//      uploadResponse: Either[VersioningException, Unit])
+//
+//    formField('version.as[PipelineVersion].optional) { version =>
+//      fileUploadAll("file") { byteSources =>
+//        formField('path.as[Path]) { path =>
+//          byteSources.map { byteSource =>
+//
+//            val contentF: Future[String] = byteSource._2.map(_.utf8String).runFold("")(_ + _)
+//            val uploadedFileF: Future[ProjectFile] = contentF.map(x => ProjectFile(path, ProjectFileContent(x)))
+//
+//            onComplete {
+//              uploadedFileF.map { uploadedFile =>
+//              for {
+//                  validateResponse <- wdlService.validateFile(uploadedFile.content)
+//                  uploadResponse <- wdlService.uploadFile(
+//                    projectId,
+//                    uploadedFile,
+//                    version,
+//                    accessToken.userId
+//                  )
+//                } yield (validateResponse, uploadResponse) match {
+//                  case (Right(_), Right(_)) => Right(StatusCodes.OK)
+//                  case (Left(_), Right(_)) => Right(StatusCodes.Created)
+//                  case (_, Left(response)) => Left(response.getMessage)
+//                }
+//              }
+//            }
+//                {
+//                  case Success(Right(sc)) => complete(sc)
+//                  case Success(Left(error)) =>
+//                    complete(StatusCodes.UnprocessableEntity, s"File have not uploaded due to $error")
+//                  case Failure(e) => complete(StatusCodes.InternalServerError, e.getMessage)
+//                }
+//              }
+//            }
+//          }.map { x =>
+//
+//          }
+//        }
+//      }
+
   val route: AccessTokenContent => Route = implicit accessToken =>
     validateFile ~
       pathPrefix("projects" / ProjectId / "files") { projectId =>
         getFile(projectId) ~
         getFiles(projectId) ~
-        uploadFile(projectId)
+        uploadFile(projectId) ~
+        uploadAllFiles(projectId)
       }
 }
